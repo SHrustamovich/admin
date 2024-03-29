2fb732e25c21fa3333f979f6c20b653e90ef4b27
Doner01 <programmer.doner2006@gmail.com>
2024-03-29 18:49:52 +0500

    fixed App

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,8 @@ import { lazy } from "react";
 
 const Dashboard = lazy(() => import("./Dashboard"));
 const Order = lazy(() => import("./Order"));
-const FlashSales = lazy(() => import("./FlashSales"));
-const Coupons = lazy(() => import("./Coupons"));
-const Pages = lazy(() => import("./Pages"));
 const Products = lazy(() => import("./Products"));
-const Users = lazy(() => import("./Users"));
-const Localizations = lazy(() => import("./Localizations"));
 const Appearance = lazy(() => import("./Appearance"));
-const Tools = lazy(() => import("./Tools"));
-const Reports = lazy(() => import("./Reports"));
 const Settings = lazy(() => import("./Settings"));
 const Sellers = lazy(() => import("./Sellers"));
 const Customers = lazy(() => import("./Customers"));
@@ -23,14 +16,7 @@ export {
     Dashboard,
     Order,
     Products,
-    FlashSales,
-    Coupons,
-    Pages,
-    Users,
-    Localizations,
     Appearance,
-    Tools,
-    Reports,
     Settings,
     Sellers,
     Customers,
